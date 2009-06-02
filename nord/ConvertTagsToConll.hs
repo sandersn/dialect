7ccadd68356ae26ad30598ad7c08bb7b1cf56456
Nathan Sanders <ncsander@indiana.edu>
2009-06-02 21:22:20 +0000

    Changed everything back to UTF-8, plus started writing berkeley parser stuff (completely unfinished, though)

--- nord/ConvertTagsToConll.hs
+++ nord/ConvertTagsToConll.hs
@@ -7,6 +7,5 @@ group = split $ dropFinalBlank $ keepDelimsR $ whenElt (".\t\t" `isPrefixOf`)
 readPos = flip withFileLines
     (filter tag & group & map (zipWith addColumns [1..]) & intercalate ["\n"])
 addColumns i = words & conllise i & intercalate "\t"
--- TODO: Add the real columns (below are guessed from Talbanken)
 conllise i [w, pos] = [show i, w, "_", pos, pos, "_", "0", "ROOT", "_", "_"]
 main = System.getArgs >>= head & readPos >>= mapM_ putStrLn
--- nord/build.py
+++ nord/build.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 0. Manually make a list of which files go with which regions.
-   All steps below operate over regions.
 
 Notes:
 SCons, in Python, replaces make and allows extensibility in Python.
@@ -16,16 +15,21 @@ import paths
 def run(cmd):
     result = os.system(cmd)
     if result: raise Exception("Error: '%s' returned code %d" % (cmd, result))
-def extractPos():
-    # 1. Extract POS tags from Talbanken
+def trainPos():
+    # 1. Train on POS tags from Talbanken
     run('ghc --make TrainPosTalbanken')
-    run('TrainPosTalbanken %s >talbanken.tt' % (' '.join(paths.talbanken),))
+    run('TrainPosTalbanken %s >talbanken.tmp.tt' % (' '.join(paths.talbanken),))
+    inf = open('talbanken.tmp.tt', encoding='latin1')
+    outf = open('talbanken.tt', 'w', encoding='utf-8')
+    for line in inf:
+        outf.write(line)
+    inf.close()
+    outf.close()
+    run('rm talbanken.tmp.tt')
 def tagPos():
     # 2. Train TnT on Talbanken
     run('tnt-para talbanken.tt')
     # 3. Extract SweDiaSyn words into TnT format
-    # - are blanks allowed?
-    # (it will make it easier to emit CoNLL files for Maltparser)
     swedia.extractTnt(paths.swpath, paths.swediaRegions)
     # 4. Tag SweDiaSyn
     for region in paths.swediaRegions:
@@ -39,11 +43,19 @@ def tagDep():
         # TODO: This must eventually depend on a config file, not command line
         # options
         run("java -Xmx256M -jar malt-1.2/malt.jar -c swemalt -i '%s.conll' -o '%s.dep.conll' -m parse" % (region, region))
+def trainCfg():
+    # n. Convert Talbanken to PTB (single-line?) for training (with uncrossing?!)
+    # Several are supported, guess I'll have to read the GrammarTrainer code
+    # n+1. Train using GrammarTrainer
+    # TODO: There are more options:  -SMcycles 5
+    run('java -cp berkeleyParser.jar edu.berkeley.nlp.PCFGLA.GrammarTrainer -path talbanken.??format -out talbanken.gr -treebank SINGLEFILE')
 def tagCfg():
     # 7. Post-process tagged SweDiaSyn to ?? format for Berkeley parser.
     # - this requires uncrossing! probably!
+    # PTB tokenised, one per line (PTB is s-exps basically)
     # 8. Constituency parse with Berkeley parser
-    return
+    for region in swediaRegions:
+        run('jar -jar berkeleyParser.jar -gr talbanken.gr <%s.ptb >%s.cfg')
 def syntaxDist():
     # 9. Run icectrl.out with various parameter settings.
     # Steal this from ice/build.py
@@ -55,7 +67,7 @@ def blade(runner, targets):
 if __name__=="__main__":
     import build
     blade(build,
-          sys.argv[1:] if sys.argv[1:] else 'extractPos tagPos'.split())
+          sys.argv[1:] if sys.argv[1:] else 'trainPos tagPos'.split())
 # TODO: Remove (or normalise to Talbanken) pauses in swedia.extractTxt
 # They look like - <first second> - also there's some funny [/] and [/-]
 
