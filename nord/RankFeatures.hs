37fe099e8efcceb0280fe638e957c96db78eddef
Nathan Sanders <ncsander@indiana.edu>
2010-01-09 14:50:05 -0500

    Possible fix for laziness causing RankFeatures to open all files at once.

--- nord/RankFeatures.hs
+++ nord/RankFeatures.hs
@@ -1,11 +1,16 @@
 import Util
-import Data.List (sort)
+import Data.List (sort, sortBy)
 import Data.List.Split (splitEvery)
-main = argsFilePrinter (withFileLines rank) id
+import Data.Ord (comparing)
+import Control.Monad (sequence, mapM_, (>=>))
+import System (getArgs)
+argsFilePrinter' :: (String -> IO [String]) -> IO ()
+argsFilePrinter' read = getArgs >>= mapM_ (read >=> mapM_ putStrLn)
+main = argsFilePrinter' (withFileLines rank)
 rank lines =
   splitEvery 2 diffs |> map tupleise |> leftright |> map format |> (header++)
   where (header,diffs) = splitAt 2 lines
         tupleise [name,value] = (name, read value :: Double)
         format (name, value) = name ++ " " ++ show value
 leftright diffs = take 5 l ++ [("************",0.0)] ++ drop (length l - 5) l
-  where l = sort diffs
+  where l = sortBy (comparing snd) diffs
