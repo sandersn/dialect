1361e1a776e9df4c0fbcc5007b42bd8b7f752206
Nathan Sanders <ncsander@jones.ling.indiana.edu>
2010-02-02 16:43:39 -0500

    Reorganised ConvertTalbankenToPTB.hs

--- nord/ConvertTalbankenToPTB.hs
+++ nord/ConvertTalbankenToPTB.hs
@@ -8,9 +8,14 @@ import Codec.Binary.UTF8.String (encodeString)
 
 type TalbankenInput = Map.Map Id FlatNode
 
+main = interactFiles (withFile posOfXml) ptbShow
+posOfXml = xmlParse "dummy" & getContent & sentences & buildSentences
+ptbShow (Leaf pos word) = "(" ++ pos ++ " " ++ word ++ ")"
+ptbShow (Node a kids) = "("++a ++" "++ unwords (map ptbShow kids)++")"
+
 sentences = tagpath ["corpus", "body", "s"]
 buildSentences :: [Content] -> [Tree String]
-buildSentences = map buildTree . filter wellformed . map buildMap
+buildSentences = map buildMap & filter wellformed & map buildTree
 buildMap :: Content -> (Id, Map.Map Id FlatNode)
 buildMap s =
   (root, Map.fromList (map termentry terms ++ map nontermentry nonterms))
@@ -27,16 +32,9 @@ buildMap s =
           ptbSafe "(" = "LParen" -- should this be LRB/RRB?
           ptbSafe ")" = "RParen"
           ptbSafe s = encodeString s
+          attrId a =  attr' a & dropWhile (/='_') & tail & read
 wellformed (root,flat) = cat (flat Map.! root) == "ROOT"
-buildTree (root,flat) = build (lookup root)
+buildTree (root,flat) = build . lookup $ root
   where build (FlatNode s word id []) = Leaf s word
         build (FlatNode s "" id kids) = Node s (map (build . lookup) kids)
         lookup = (flat Map.!)
-parseId :: String -> (String, Integer)
-parseId s = (takeWhile (/='_') s, read . tail . dropWhile (/='_') $ s)
-attrId a = snd . parseId . attr' a
-parseTalbanken filename = withFile (posOfXml filename) filename
-posOfXml filename = xmlParse filename & getContent & sentences & buildSentences
-ptbShow (Leaf pos word) = "(" ++ pos ++ " " ++ word ++ ")"
-ptbShow (Node a kids) = "("++a ++" "++ unwords (map ptbShow kids)++")"
-main = interactFiles parseTalbanken ptbShow
