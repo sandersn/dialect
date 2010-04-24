0f3b5b0f1038a49f01dd4be0250abc294c9779e0
Nathan Sanders <ncsander@indiana.edu>
2010-04-24 15:40:39 -0400

    Got some notes for feature ranking analysis. Changed FormatFeatures to use pstricks and colour.

--- nord/FormatFeatures.hs
+++ nord/FormatFeatures.hs
@@ -3,35 +3,59 @@ import System
 import Text.Printf (printf)
 import Control.Arrow (second)
 import Data.List.Split (chunk)
-fig = "\\begin{figure}\n\
-      \\\setlength{\\unitlength}{1pt}\n\
-      \\\begin{picture}(400,140)\n\
-      \\\linethickness{2mm}\n\
-      \%s\
-      \\\end{picture}\n\
-      \\\caption{%s %s}\n\
-      \\\end{figure}\n\n"
-main = interactFiles (withFileLines parse) format
-  where tmp (s,_,_,_) = s
+-- latex clusterA-clusterD-feat-5-1000-r-trigram-ratio.txt && dvips -Ppdf clusterA-clusterD-feat-5-1000-r-trigram-ratio.dvi && ps2pdf clusterA-clusterD-feat-5-1000-r-trigram-ratio.ps && open clusterA-clusterD-feat-5-1000-r-trigram-ratio.pdf
+fig = "\\documentclass{article}\n\
+       \\\usepackage[dvips]{geometry}\n\
+       \\\usepackage{pstricks}\n\
+       \\\geometry{papersize={85mm,45mm},total={85mm,45mm}}\n\
+       \\\addtolength{\\topmargin}{-1.4in}\n\
+       \\\addtolength{\\textheight}{3.6in}\n\
+       \\\setlength{\\parindent}{0cm}\n\
+       \\\pagestyle{empty}\n\
+       \\\begin{document}\n\
+       \\\thispagestyle{empty}\n\
+       \\\definecolor{LeftColor}{hsb}{0.9,0.87,1}\n\
+       \\\definecolor{RightColor}{hsb}{1.0,0.86,1}\n\
+       \\\psset{linewidth=4pt}\n\
+       \\\begin{pspicture}(%f,8.0)\n\
+       \ %s\n\
+       \\\end{pspicture}\n\
+       \%%\\setlength{\\unitlength}{1pt}\n\
+       \\\end{document}\n\n"
+maxwidth = 3.0
+maxheight = 3.20
+offset = 3.0
+main = do
+  [f] <- getArgs
+  readFile f >>= lines & parse & return >>= mapM_ (output f)
+output f (from,to,ns) = format ns |> writeFile (printf "%s-%s-%s" from to f)
 parse = chunk 13 & map struct
-struct (from:to:numbers) = (from,to,take 5 ns++drop 6 ns)
+struct (from:to:numbers) = (shorten from,shorten to,take 5 ns++drop 6 ns)
  where ns' = map (words & parse) numbers
        ns = scale largest ns'
        parse [n] = ("", read n :: Double)
        parse [f,n] = (f, read n)
        largest = maximum (map (abs . snd) ns')
        scale largest = map (second scale')
-       scale' n = n / largest * 180.0
-format :: (String,String,[(String,Double)]) -> String
-format (from,to,ns) = printf fig (fmt ns) from to
-  where fmt l = unlines (map formatLine (zip [0..] l))
-formatLine :: (Integer,(String,Double)) -> String
+       scale' n = n / largest * maxwidth
+       shorten = takeWhile (/='-')
+format :: [(String,Double)] -> String
+format ns = printf fig maxheight (fmt ns)
+  where fmt l = unlines (map formatLine (zip (map fromIntegral [0..]) l))
+formatLine :: (Double,(String,Double)) -> String
 formatLine (i,(pos,n)) =
-  printf "\\put(%d,%d){\\line(%s,0){%f}}\n\
-          \\\put(190,%d){%s}"
-    (if n < 0 then 180 else 250 :: Integer)
+  printf "  \\uput{0.2}[0](0,%f){%s}\n\
+         \  \\psline[linecolor=%s](%f,%f)(%f,%f)\n\
+         \  \\uput{0.2}[0](7,%f){%.2f}\n"
+    height pos
+    (if n < 0 then "RightColor" else "LeftColor")
+    offset height (offset + abs n) height height n
+  where height = maxheight - i * (maxheight / 10)
+    {-(if n < 0 then 180 else 250 :: Integer)
     (110 - i * 11)
     (if n < 0 then "-1" else "1")
     (abs n)
     (107 - i * 11)
     pos
+"\\put(%d,%d){\\line(%s,0){%f}}\n\
+          \\\put(190,%d){%s}" -}
